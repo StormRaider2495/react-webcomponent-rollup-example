fed835a1e5dce6749b5425b9d0ab3fbc610e8af3
arunitmazumdar <arunit.mazumdar@learningmate.com>
2021-01-29 10:55:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 import './index.css';
-import App from './App';
+import ReactWebComponent from 'react-web-component';
 import reportWebVitals from './reportWebVitals';
+import './mycomponent';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Greeting extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello, {this.props.name}</h1>
+        <div className="container">
+          <my-counter></my-counter>
+        </div>
+      </>
+    );
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+ReactWebComponent.create(<Greeting name="Mars"></Greeting>, 'web-greeting');
+
+
---
+++ src/mycomponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactWebComponent from 'react-web-component';
+import './mycomponent.css';
+
+class MyCounter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  increment() {
+    this.setState((prev) => ({
+      count: prev.count + 1
+    }));
+  }
+  decrement() {
+    this.setState((prev) => ({
+      count: prev.count - 1
+    }));
+  }
+  render() {
+    return (
+      <div className='counter-container'>
+        <h1>Example of Nested React Web Component</h1>
+        <button onClick={() => this.increment()}>up</button>
+        <button onClick={() => this.decrement()}>down</button>
+        <span>{this.state.count}</span>
+      </div>
+    );
+  }
+}
+
+ReactWebComponent.create(<MyCounter/>, 'my-counter');
